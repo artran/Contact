f70e5d6d268064c7963c9ae9db197014b78c59a2
Ray Tran <ray@artran.co.uk>
2010-06-15 22:40:33 +0100

    Made the view work.

--- views.py
+++ views.py
@@ -1,4 +1,8 @@
+from django.conf import settings
+from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
+from django.template.loader import render_to_string
 
 from forms import ContactForm
 from models import *
@@ -9,6 +13,9 @@ def contact(request, form_id):
         form = ContactForm(form_id, request.POST) # A form bound to the POST data
         if form.is_valid(): # All validation rules pass
             # Process the data in form.cleaned_data
+            body = render_to_string('contact/contact.eml', {'post': request.POST})
+            send_mail('Customer email from website', body, settings.DEFAULT_FROM_EMAIL,
+                settings.CONTACT_RECIPIENTS, fail_silently=False)
             return HttpResponseRedirect('/thanks/') # Redirect after POST
     else:
         form = ContactForm(form_id) # An unbound form
